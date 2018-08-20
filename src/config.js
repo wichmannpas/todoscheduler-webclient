ab53b7cd9139b55fe8b3c830e5f12c31da14acc7
Pascal Wichmann <git@pa-w.de>
2018-08-20 19:11:44 +0200

    Add configurable imprint page

--- src/config.js
+++ src/config.js
@@ -1,5 +1,10 @@
 const API_URL = 'http://127.0.0.1:8000'
 
+const USE_IMPRINT = true
+const IMPRINT_ADDRESS = 'John Doe\nFoo Street 42\n1337 Bar City\nGermany'
+
 export {
-  API_URL
+  API_URL,
+  USE_IMPRINT,
+  IMPRINT_ADDRESS
 }
