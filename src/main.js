efb79e134c74c323c81fa827e96eb4dd2d8c1126
Pascal Wichmann <git@pa-w.de>
2018-08-23 14:25:47 +0200

    Use spectre.css from npm

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App'
 import router from './router'
 import store from './store'
 
-import '@/assets/css/spectre.css'
+import 'spectre.css/dist/spectre.css'
 
 import '@/assets/scss/base.scss'
 import '@/icons'
