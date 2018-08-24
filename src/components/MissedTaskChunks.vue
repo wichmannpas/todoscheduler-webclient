38dca0b096196e433ba020947ecc26d0e8892843
Pascal Wichmann <git@pa-w.de>
2018-08-24 19:46:29 +0200

    Use dedicated loading spinner for each component to speed up displaying of faster components

--- src/components/MissedTaskChunks.vue
+++ src/components/MissedTaskChunks.vue
@@ -1,30 +1,37 @@
 <template>
   <div
-      v-if="missedTaskChunks.length > 0"
-      class="toast toast-warning">
-    There are unfinished scheduled tasks for past days!
+      v-if="ready">
+    <div
+        v-if="missedTaskChunks.length > 0"
+        class="toast toast-warning">
+      There are unfinished scheduled tasks for past days!
 
-    <ul>
-      <li
-          v-for="chunk in missedTaskChunks"
-          v-bind:key="chunk.id">
-          {{ chunk.task($store).name }} ({{ chunk.naturalDay() }})
-          <a
-              @click="finishChunk(chunk)"
-              class="tooltip"
-              data-tooltip="Done">
-            <font-awesome-icon
-                icon="check" />
-          </a>
-          <a
-              @click="postponeChunk(chunk)"
-              class="tooltip"
-              data-tooltip="Postpone to another day">
-            <font-awesome-icon
-                :icon="['far', 'clock']" />
-          </a>
-      </li>
-    </ul>
+      <ul>
+        <li
+            v-for="chunk in missedTaskChunks"
+            v-bind:key="chunk.id">
+            {{ chunk.task($store).name }} ({{ chunk.naturalDay() }})
+            <a
+                @click="finishChunk(chunk)"
+                class="tooltip"
+                data-tooltip="Done">
+              <font-awesome-icon
+                  icon="check" />
+            </a>
+            <a
+                @click="postponeChunk(chunk)"
+                class="tooltip"
+                data-tooltip="Postpone to another day">
+              <font-awesome-icon
+                  :icon="['far', 'clock']" />
+            </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div
+      v-else
+      class="loading loading-lg">
   </div>
 </template>
 
@@ -34,6 +41,9 @@ import { deleteTaskChunk, finishTaskChunk } from '@/api/taskchunk'
 export default {
   name: 'MissedTaskChunks',
   computed: {
+    ready () {
+      return this.$store.state.task.ready
+    },
     missedTaskChunks () {
       return this.$store.getters.missedTaskChunks
     }
