bc999376f49801752baeb1a12afbeb6b97341759
Pascal Wichmann <git@pa-w.de>
2018-08-27 13:10:57 +0200

    Move imprint link to drawer

--- src/App.vue
+++ src/App.vue
@@ -1,100 +1,22 @@
 <template>
   <div class="app">
-    <aside
-        ref="drawer"
-        class="mdc-drawer mdc-drawer--temporary mdc-typography">
-      <nav class="mdc-drawer__drawer">
-        <header class="mdc-drawer__header">
-          <div class="mdc-drawer__header-content">
-            TodoScheduler
-          </div>
-        </header>
-        <nav id="icon-with-text-demo" class="mdc-drawer__content mdc-list">
-          <a class="mdc-list-item mdc-list-item--activated" href="#">
-            <i class="material-icons mdc-list-item__graphic" aria-hidden="true">inbox</i>Inbox
-          </a>
-          <a class="mdc-list-item" href="#">
-            <i class="material-icons mdc-list-item__graphic" aria-hidden="true">star</i>Star
-          </a>
-        </nav>
-      </nav>
-    </aside>
-    <header
-        ref="topBar"
-        class="mdc-top-app-bar mdc-top-app-bar--fixed">
-      <div class="mdc-top-app-bar__row">
-        <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
-          <a
-            ref="menuButton"
-            class="
-                c-hand
-                material-icons
-                mdc-top-app-bar__navigation-icon">
-            menu
-          </a>
-          <span class="mdc-top-app-bar__title">
-            TodoScheduler
-          </span>
-        </section>
-        <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
-          <a href="#" class="material-icons mdc-top-app-bar__action-item" aria-label="Bookmark this page" alt="Bookmark this page">bookmark</a>
-        </section>
-      </div>
-    </header>
+    <TopAppBar />
 
-    <div>
-      <router-view />
-    </div>
+    <router-view />
 
-    <div class="footer">
-      <hr />
-      <a href="https://github.com/wichmannpas/todoscheduler">
-        TodoScheduler</a>
-      is free software.
-      &bull;
-      <router-link :to="{ name: 'main' }">Home</router-link>
-      <span v-if="useImprint">
-        &bull;
-        <router-link :to="{ name: 'imprint' }">Imprint</router-link>
-      </span>
-    </div>
-
-    <div
-        ref="snackbar"
-        class="mdc-snackbar"
-        aria-live="assertive"
-        aria-atomic="true"
-        aria-hidden="true">
-      <div class="mdc-snackbar__text">
-      </div>
-      <div class="mdc-snackbar__action-wrapper">
-        <button type="button" class="mdc-snackbar__action-button"></button>
-      </div>
-    </div>
+    <Snackbar />
   </div>
 </template>
 
 <script>
-import { drawer, snackbar, topAppBar } from 'material-components-web'
-
-import { USE_IMPRINT } from '@/config'
-import { initSnackbar } from '@/snackbar'
+import Snackbar from '@/components/Snackbar'
+import TopAppBar from '@/components/TopAppBar'
 
 export default {
   name: 'App',
-  data: function () {
-    return {
-      ui: {
-        drawer: null,
-        snackbar: null,
-        topBar: null
-      }
-    }
-  },
-  computed: {
-    useImprint: function () {
-      return USE_IMPRINT
-    }
+  components: {
+    Snackbar,
+    TopAppBar
   },
   created: function () {
     window.handleMissingAuth = () => {
@@ -107,23 +29,6 @@ export default {
       }
 
       this.$store.commit('reset')
-    }
-  },
-  mounted: function () {
-    if (this.ui.topBar === null) {
-      this.ui.topBar = new topAppBar.MDCTopAppBar(this.$refs.topBar)
-    }
-
-    if (this.ui.drawer === null) {
-      this.ui.drawer = new drawer.MDCTemporaryDrawer(this.$refs.drawer)
-      this.$refs.menuButton.addEventListener('click', () => {
-        this.ui.drawer.open = true
-      })
-    }
-
-    if (this.ui.snackbar === null) {
-      this.ui.snackbar = new snackbar.MDCSnackbar(this.$refs.snackbar)
-      initSnackbar(this.ui.snackbar)
     }
   }
 }
