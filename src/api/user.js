c213d082763fb82495a0bd3534b6467baf71f8ea
Pascal Wichmann <git@pa-w.de>
2018-09-01 17:04:36 +0200

    Implement user settings page

--- src/api/user.js
+++ src/api/user.js
@@ -20,6 +20,36 @@ function fetchUser () {
   })
 }
 
+/**
+ * Update the user settings.
+ * data may contain arbitrary attributes of a user. These are:
+ * * workhoursWeekday
+ * * workhoursWeekend
+ * * defaultScheduleDuration
+ * * defaultScheduleFullDurationMax
+ * * password
+ *
+ * Additional attributes are ignored
+ */
+function updateUser (data) {
+  return new Promise(function (resolve, reject) {
+    if (!ensureAuthToken()) {
+      reject(new Error('no auth'))
+    }
+
+    axios.patch(API_URL + '/base/user/', {
+      workhours_weekday: data.workhoursWeekday,
+      workhours_weekend: data.workhoursWeekend,
+      default_schedule_duration: data.defaultScheduleDuration,
+      default_schedule_full_duration_max: data.defaultScheduleFullDurationMax,
+      password: data.password
+    }).then((response) => {
+      return resolve(deserialize(User, response.data))
+    }).catch(error => handleGenericErrors(error, resolve, reject))
+  })
+}
+
 export {
-  fetchUser
+  fetchUser,
+  updateUser
 }
---
+++ src/pages/UserSettings.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h1>
+      User Settings
+    </h1>
+
+    <p>
+      You can change the settings of your user account on this page.
+    </p>
+
+    <UserForm
+        :user="$store.state.user.user" />
+  </div>
+</template>
+
+<script>
+import UserForm from '@/components/UserForm'
+
+export default {
+  name: 'UserSettings',
+  components: {
+    UserForm
+  }
+}
+</script>
