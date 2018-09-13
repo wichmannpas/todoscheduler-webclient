4fc586962dc3eeb597822ce669bac2270c9c13d6
Pascal Wichmann <git@pa-w.de>
2018-09-13 10:38:18 +0200

    Series: Update task duration when receiving cleaned task chunks (closes #122)

--- src/store/taskchunkseries.js
+++ src/store/taskchunkseries.js
@@ -1,3 +1,4 @@
+import Decimal from 'decimal.js-light'
 import Vue from 'vue'
 
 import { deserialize } from 'serializr'
@@ -5,6 +6,7 @@ import { deserialize } from 'serializr'
 import { fetchTaskChunkSeries } from '@/api/taskchunk'
 import Task from '@/models/Task'
 import TaskChunk from '@/models/TaskChunk'
+import TaskChunkSeries from '@/models/TaskChunkSeries'
 
 export default {
   state: {
@@ -57,7 +59,11 @@ export default {
       })
     },
     addRawTaskChunksFromSeries ({ commit, rootState }, responseData) {
+      let updatedTask = false
+
       let newTaskChunks = responseData.scheduled.map(raw => deserialize(TaskChunk, raw))
+
+      let task
 
       if (newTaskChunks.length > 0) {
         commit('addUpdateTaskChunks', {
@@ -67,14 +73,39 @@ export default {
 
         // at least one new chunk was scheduled and the task updated
         // we extract the task from the first scheduled chunk and update it
-        let task = deserialize(Task, responseData.scheduled[0].task)
+        task = deserialize(Task, responseData.scheduled[0].task)
         commit('updateTask', {
           task: task,
           today: rootState.time.today
         })
+        updatedTask = true
       }
 
       if (responseData.cleaned !== undefined) {
+        let series = deserialize(TaskChunkSeries, responseData.series)
+
+        if (!updatedTask) {
+          task = rootState.task.tasks[responseData.series.task_id]
+          // reduce the duration of the task by the duration of the cleaned task chunks
+          let reducedDuration = Decimal(0)
+          responseData.cleaned.forEach(chunkId => {
+            let chunk = rootState.taskchunk.taskChunks[chunkId]
+            let duration
+            if (chunk === undefined) {
+              console.warn('cleaned task chunk which we do not now, assuming default duration')
+              duration = series.duration
+            } else {
+              duration = chunk.duration
+            }
+            reducedDuration = reducedDuration.add(duration)
+          })
+          task.duration = task.duration.sub(reducedDuration)
+          commit('updateTask', {
+            task: task,
+            today: rootState.time.today
+          })
+        }
+
         commit('deleteTaskChunks', responseData.cleaned)
       }
     }
